843bd902e085b429ec5faf23e525cbd5b2133523
panwanting <pan19950529@gmail.com>
2016-08-19 15:09:53 +0800

    app2 BMI

--- ProjectName/index.android.js
+++ ProjectName/index.android.js
@@ -5,24 +5,64 @@
  */
 
 import React, { Component } from 'react';
-import {
+import {  //要使用標籤記得現在這邊輸入該標籤import
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 class ProjectName extends Component {
-  render() {
+  
+  constructor(props){
+    super(props);
+    this.state={ text1: 'height', //text1的預設值是123 
+                text2: 'weight', //text2的預設值是456
+                text3: 'BMI'
+    }
+  }
+  
+  onPress = () => {        
+        console.log('pressed');  
+        
+        var h = this.state.text1 / 100 ; 
+        var w = this.state.text2 ;
+        var bmi = w/(h*h) ;
+        this.setState({text3:bmi})
+  }
+  
+  render() { //若textinput有預設值的話 一定要用onchangetext依照新輸入的值去做更新 否則無法取新輸入的值
     return (
-      <View style={styles.container}>
+     
+      <View style={styles.container}> 
+       <Text>Height:(please enter cm)</Text><TextInput
+        style={{height: 40, borderColor: 'gray', width: 80}}
+        onChangeText={(text1) => this.setState({text1})}  //框框輸入的text1去setState更新constructor裡面預設的text1
+        value={this.state.text1} //原本預設的123會被更新成框框輸入的  
+       /> 
+       <Text>Weight:</Text>
+       <TextInput
+        style={{height: 40, borderColor: 'gray', width: 80}}
+        onChangeText={(text2) => this.setState({text2})} 
+        value={this.state.text2} 
+       />
+      
+        <TouchableOpacity style={{backgroundColor: 'gray'}} onPress={this.onPress}>
+          <Text>cal BMI</Text> 
+        </TouchableOpacity>
+        
+        <Text>BMI is {this.state.text3}</Text> 
+  
+       
         <Text style={styles.welcome}>
-          Welcome to React Native! yoyo
+          Welcome to React Native! yoyoyoyoyo
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={styles.instructions} numberOfLines={1}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
