96e40303ad9b3b5f921e349ab3ec1adda189f473
panwanting <pan19950529@gmail.com>
2016-08-19 15:05:01 +0800

    app2 BMI

--- ProjectName/index.android.js
+++ ProjectName/index.android.js
@@ -10,38 +10,51 @@ import {  //要使用標籤記得現在這邊輸入該標籤import
   StyleSheet,
   Text,
   View,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 class ProjectName extends Component {
   
   constructor(props){
     super(props);
-    this.state={ text1: '123', //text1的預設值是123 
-                text2: '456' //text2的預設值是456
+    this.state={ text1: 'height', //text1的預設值是123 
+                text2: 'weight', //text2的預設值是456
+                text3: 'BMI'
     }
-  
   }
   
+  onPress = () => {        
+        console.log('pressed');  
+        
+        var h = this.state.text1 / 100 ; 
+        var w = this.state.text2 ;
+        var bmi = w/(h*h) ;
+        this.setState({text3:bmi})
+  }
   
   render() { //若textinput有預設值的話 一定要用onchangetext依照新輸入的值去做更新 否則無法取新輸入的值
     return (
      
       <View style={styles.container}> 
-       <TextInput
-        style={{height: 40, borderColor: 'gray', Width: 10}}
+       <Text>Height:(please enter cm)</Text><TextInput
+        style={{height: 40, borderColor: 'gray', width: 80}}
         onChangeText={(text1) => this.setState({text1})}  //框框輸入的text1去setState更新constructor裡面預設的text1
         value={this.state.text1} //原本預設的123會被更新成框框輸入的  
        /> 
+       <Text>Weight:</Text>
        <TextInput
-        style={{height: 40, borderColor: 'gray', Width: 10}}
+        style={{height: 40, borderColor: 'gray', width: 80}}
         onChangeText={(text2) => this.setState({text2})} 
         value={this.state.text2} 
        />
-       
-        <Text>
-          {this.state.text1} {this.state.text2}
-        </Text> 
+      
+        <TouchableOpacity style={{backgroundColor: 'gray'}} onPress={this.onPress}>
+          <Text>cal BMI</Text> 
+        </TouchableOpacity>
+        
+        <Text>BMI is {this.state.text3}</Text> 
+  
        
         <Text style={styles.welcome}>
           Welcome to React Native! yoyoyoyoyo
